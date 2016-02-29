26c07616b7d11c7179d5e4a156909257fcc6cb5c
subashsaran <msubas71@gmail.com>
2016-03-01 02:47:50 +0530

    second_commit

--- server.R
+++ server.R
@@ -15,12 +15,12 @@ function(input, output) {
         )
         #View the dataset in table
         output$diamond_table <- renderDataTable({
-                trainData
+                diamonds
         }, options = list(bSortClasses = TRUE))
         #Plotting
         output$plot <- renderPlot({
                 
-                p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
+                p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_bar(stat = "identity")
                 
                 if (input$color != 'None')
                         p <- p + aes_string(color=input$color)
@@ -29,10 +29,10 @@ function(input, output) {
                 if (facets != '. ~ .')
                         p <- p + facet_grid(facets)
                 
-                if (input$jitter)
-                        p <- p + geom_jitter()
-                if (input$smooth)
-                        p <- p + geom_smooth()
+#                 if (input$jitter)
+#                         p <- p + geom_jitter()
+#                 if (input$smooth)
+#                         p <- p + geom_smooth()
                 
                 print(p)
                 
